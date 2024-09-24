67091dac85ed848ffe9de248b17fa3842c067ed9
Navi2010 <99569813+Navi2010@users.noreply.github.com>
2024-09-24 17:22:22 -0500

    Add files via upload

---
+++ bill.py
@@ -0,0 +1,6 @@
+amount_due = float(input('enter the amount that was due: '))
+amount_payed = float(input('enter the amount you payed: '))
+
+x = amount_payed - amount_due
+
+print ('you get', x, ' dollars back.')
